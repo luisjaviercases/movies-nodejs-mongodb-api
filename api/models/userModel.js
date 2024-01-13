5f0a5546d04b898bf42f9defe8b80ae3cf3e561c
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-13 14:18:04 +0100

    feat(models): create userModel

---
+++ api/models/userModel.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+/**
+ * User Schema
+ */
+const UserSchema = new Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true,
+  },
+  hash_password: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.hash_password);
+};
+
+const UserModel = mongoose.model('User', UserSchema);
+
+export default UserModel;
