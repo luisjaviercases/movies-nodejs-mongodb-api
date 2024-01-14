5cf77e713fb398142c89224e389b139de80d59a7
Luis Javier Cases <l.cases@cathedralsw.com>
2024-01-14 14:05:15 +0100

    feat(routes): add new movies endpoints
    User could add favoriteMovies, remove or get them

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 import { loginRequired, register, sign_in } from '../controllers/userController.js';
-import { getMovies, getMovieById } from '../controllers/movieController.js';
+import { getMovies, getMovieById, getFavoriteMovies, addFavoriteMovie, removeFavoriteMovie } from '../controllers/movieController.js';
 import { getGenres } from '../controllers/genreController.js';
 
 export default function (app) {
@@ -14,6 +14,12 @@ export default function (app) {
   .get(loginRequired, getMovies);
   app.route('/films/movies/:id')
     .get(loginRequired, getMovieById);
+  app.route('/films/user')
+    .get(loginRequired, getFavoriteMovies);
+  app.route('/films/user/list')
+    .post(loginRequired, addFavoriteMovie);
+  app.route('/films/user/list/:id')
+    .delete(loginRequired, removeFavoriteMovie);
 
   // Genre routes
   app.route('/films/genres')
